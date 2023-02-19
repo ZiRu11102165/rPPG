b94b50f2d67fa9c1cff22025da38b1cf16a47a19
ZiRu11102165 <124028666+ZiRu11102165@users.noreply.github.com>
2023-02-20 00:36:27 +0800

    Create test.py

---
+++ model/test.py
@@ -0,0 +1,54 @@
+# 第一個(Epoch:50 , loss: 9.0821 - mae: 2.2701)
+def Model():
+    model = Sequential()
+    model.add(Conv1D(filters=20,kernel_size=9,strides=1,input_shape=(250,1),padding="SAME",activation = 'relu'))  # filters空間維度,kernel_size卷積窗口的長度,strides卷積的步長,
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=4,padding="SAME"))   #pool_size窗口大小,strides縮小比例的因數,padding: "valid" 或者 "same"
+    model.add(Dropout(rate =0.1))
+    model.add(Conv1D(filters=20,kernel_size=9,strides=1,padding="SAME",activation = 'relu'))  # filters空間維度,kernel_size卷積窗口的長度,strides卷積的步長,
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=4,padding="SAME"))   #pool_size窗口大小,strides縮小比例的因數,padding: "valid" 或者 "same"
+    model.add(Dropout(rate =0.1))
+    
+    model.add(Bidirectional(GRU(64, return_sequences=True)))
+    model.add(Dropout(rate=0.1))
+    model.add(Bidirectional(GRU(128)))
+    model.add(Dropout(rate=0.1))
+    model.add(Flatten())
+    model.add(Dense(2, activation='relu'))
+    return model
+model = Model()
+opt = Adam(learning_rate=0.001)
+model.compile(loss='mean_squared_error',optimizer=opt,metrics=['mae'])
+batch_size = 32
+epochs = 50
+seed = 7
+X_train, x_test, Y_train, y_test = train_test_split(X_train, Y_train, test_size=0.33, random_state=seed)
+record = model.fit( X_train, Y_train,validation_data=(x_test,y_test),batch_size=batch_size,epochs=epochs,verbose=1,shuffle=True,)	
+# 第一個
+
+# 第二個
+def Model():
+    model = Sequential()
+    model.add(Conv1D(filters=20,kernel_size=9,strides=1,input_shape=(250,1),padding="SAME",activation = 'relu'))  # filters空間維度,kernel_size卷積窗口的長度,strides卷積的步長,
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=4,padding="SAME"))   #pool_size窗口大小,strides縮小比例的因數,padding: "valid" 或者 "same"
+    model.add(Dropout(rate =0.1))
+    model.add(Conv1D(filters=20,kernel_size=9,strides=1,padding="SAME",activation = 'relu'))  # filters空間維度,kernel_size卷積窗口的長度,strides卷積的步長,
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=4,padding="SAME"))   #pool_size窗口大小,strides縮小比例的因數,padding: "valid" 或者 "same"
+    model.add(Dropout(rate =0.1))
+    
+    model.add(Bidirectional(ConvLSTM1D(64, return_sequences=True)))
+    model.add(Dropout(rate=0.1))
+    model.add(Bidirectional(ConvLSTM1D(128)))
+    model.add(Dropout(rate=0.1))
+    model.add(Flatten())
+    model.add(Dense(2, activation='relu'))
+    return model
+model = Model()
+# 第二個
+
+# 第三個
+
+# 第三個
