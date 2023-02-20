e4ab21162be2eb044bac599bb97747683961dfb6
ZiRu11102165 <124028666+ZiRu11102165@users.noreply.github.com>
2023-02-20 19:21:47 +0800

    Update test_0220.py

--- model/test_0220.py
+++ model/test_0220.py
@@ -56,24 +56,6 @@ gpu_devices = tf.config.experimental.list_physical_devices("GPU")
 for device in gpu_devices:
     tf.config.experimental.set_memory_growth(device, True)
 
-# class AttentionLayer(Layer):
-#     def __init__(self, **kwargs):
-#         super(AttentionLayer, self).__init__(**kwargs)
-
-#     def build(self, input_shape):
-#         self.W = self.add_weight(name="att_weight", shape=(input_shape[-1], 1), initializer="normal")
-#         self.b = self.add_weight(name="att_bias", shape=(input_shape[1], 1), initializer="zeros")
-#         super(AttentionLayer, self).build(input_shape)
-
-#     def call(self, x):
-#         e = K.tanh(K.dot(x, self.W) + self.b)
-#         a = K.softmax(e, axis=1)
-#         output = x * a
-#         return K.sum(output, axis=1)
-
-#     def compute_output_shape(self, input_shape):
-#         return (input_shape[0], input_shape[-1])
-
 def Model():
     model = Sequential()
     model.add(Conv1D(filters=20,kernel_size=9,strides=1,input_shape=(250,1),padding="SAME",activation = 'relu'))  # filters空間維度,kernel_size卷積窗口的長度,strides卷積的步長,
@@ -127,13 +109,8 @@ plt.savefig('model.png',dpi=300,format='png')
 print('Result saved into model.png')
 
 y_pred = model.predict(x_test)
-# print(x_test)
 print(y_pred)
 print(y_test)
-# print(type(y_test))
-# y_test_out = np.array(y_test)
-# for i in y_test_out:
-#     print(y_test_out[i][0])
 
 pd.DataFrame(y_pred).to_csv('y_pred.csv')
 pd.DataFrame(y_test).to_csv('y_test.csv')
